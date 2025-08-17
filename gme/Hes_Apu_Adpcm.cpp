478129201eeb154fb3dace11e385ea6560c4bdd6
Mike Will <myQwil@gmail.com>
2025-08-17 12:35:18 -0400

    Use nullptr for all null pointer constants
    Also add `-Wzero-as-null-pointer-constant` to cxx flags.

--- gme/Hes_Apu_Adpcm.cpp
+++ gme/Hes_Apu_Adpcm.cpp
@@ -19,7 +19,7 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA */
 
 Hes_Apu_Adpcm::Hes_Apu_Adpcm()
 {
-	output = NULL;
+	output = nullptr;
 
 	memset( &state, 0, sizeof( state ) );
 
